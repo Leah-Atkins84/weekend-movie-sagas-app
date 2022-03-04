d4abebe9e74c70f3569481753923c83d9ddb9269
Leah Atkins <leah.atkins84@gmail.com>
2022-03-04 17:06:11 -0600

    Added components and installations

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,9 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
+import DetailsPage from '../DetailsPage/DetailsPage';
+import AddMovie from '../AddMovie/AddMovie';
+
 
 function App() {
   return (
@@ -12,8 +15,14 @@ function App() {
         </Route>
         
         {/* Details page */}
-
+        <Route path="/DetailsPage" exact>
+          <DetailsPage />
+        </Route>
+        
         {/* Add Movie page */}
+        <Route path="/AddMovie" exact>
+          <AddMovie />
+        </Route>
       </Router>
     </div>
   );
