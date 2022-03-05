5628da28638965b2b586e8a903babab0ba5da6f3
Leah Atkins <leah.atkins84@gmail.com>
2022-03-05 17:34:09 -0600

    Added button to go back to main page and code comments to details page

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -11,22 +11,23 @@ const handleClick = () => {
 const movie = useSelector(store => store.details);
 const genre = useSelector(store => store.genres);
 
+// movie details page lists movie description, name, image and genres
     return (
         <main>
-        <h1>Movie Details</h1>    
+        <h1>Movie Details</h1>   
                     <div key={movie.id}>
                         <h3>{movie.title}</h3>
                         <img src={movie.poster} alt={movie.title}/>
                         <p>{movie.description}</p>
                     </div>
-                    <h3>Genres: </h3>
+                    <h3>Genres: </h3> 
                     {genre.map(genre => (
                          <h4 key={genre.name}>  
                          {genre.name}</h4>   
-                    ))} 
+                    ))}
                     <button onClick={handleClick}>Back to Movie List Page</button>         
         </main> 
     )
- }
+ }// end DetailsPage
 
 export default DetailsPage;
